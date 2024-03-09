b916eccbf38bbd6f57838ba7c19420b365da4e63
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2024-03-09 21:52:49 +1300

    hello world

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 
 /**
  * The main function for the action.
@@ -11,6 +12,7 @@ async function run() {
     console.log(`Hello ${nameToGreet}!`)
     const time = new Date().toTimeString()
     core.setOutput('time', time)
+
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`)
