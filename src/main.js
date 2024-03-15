69086e644eef2c6b3e880982c07f5db72ba5171f
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2024-03-15 21:35:46 +1300

    initial action

--- src/main.js
+++ src/main.js
@@ -1,26 +1,179 @@
+import { encrypt } from './util'
+
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { Octokit, App } = require('octokit')
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
-  try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello ${nameToGreet}!`)
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
+    try {
+        // Get the input from the action
+        const environmentName = core.getInput('environment-name')
+        const isSecret = core.getInput('is-secret')
+        const name = core.getInput('name')
+        const value = core.getInput('value')
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+        // Get context from the action
+        const owner = github.context.repo.owner
+        const repo = github.context.repo.repo
+        const token = github.context.token
+
+        // Create GitHub client
+        const octokit = new Octokit({ auth: token })
+
+        // Encrypt the value if secret
+        let publicKeyValue
+        let publicKeyId
+        let encryptedValue
+        if (isSecret) {
+            const response = await octokit.request(
+                'GET /repos/{owner}/{repo}/actions/secrets/public-key',
+                {
+                    owner: owner,
+                    repo: repo,
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                }
+            )
+            const data = response.data
+
+            publicKeyValue = data.key
+            publicKeyId = data.key_id
+            encryptedValue = encrypt(publicKeyValue, value)
+        }
+
+        if (!environmentName) {
+            // Repository
+            if (isSecret) {
+                // Secret
+                await octokit.request(
+                    'PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}',
+                    {
+                        owner: owner,
+                        repo: repo,
+                        secret_name: name,
+                        encrypted_value: encryptedValue,
+                        key_id: publicKeyId
+                    }
+                )
+            } else {
+                // Variable
+                const response = await octokit.request(
+                    'GET /repos/{owner}/{repo}/actions/variables/{name}',
+                    {
+                        owner: owner,
+                        repo: repo,
+                        name: name,
+                        headers: {
+                            'X-GitHub-Api-Version': '2022-11-28'
+                        }
+                    }
+                )
+                const status = response.status
+
+                if (status === 200) {
+                    // Update
+                    await octokit.request(
+                        'PATCH /repos/{owner}/{repo}/actions/variables/{name}',
+                        {
+                            owner: owner,
+                            repo: repo,
+                            name: name,
+                            value: value,
+                            headers: {
+                                'X-GitHub-Api-Version': '2022-11-28'
+                            }
+                        }
+                    )
+                } else {
+                    // Create
+                    await octokit.request(
+                        'POST /repos/{owner}/{repo}/actions/variables',
+                        {
+                            owner: owner,
+                            repo: repo,
+                            name: name,
+                            value: value,
+                            headers: {
+                                'X-GitHub-Api-Version': '2022-11-28'
+                            }
+                        }
+                    )
+                }
+            }
+        } else {
+            // Environment
+            if (isSecret) {
+                // Secret
+                await octokit.request(
+                    'PUT /repos/{owner}/{repo}/environments/{environment_name}/secrets/{secret_name}',
+                    {
+                        owner: owner,
+                        repo: repo,
+                        environment_name: environmentName,
+                        secret_name: name,
+                        encrypted_value: encryptedValue,
+                        key_id: publicKeyId
+                    }
+                )
+            } else {
+                // Variable
+                const response = await octokit.request(
+                    'GET /repos/{owner}/{repo}/environments/{environment_name}/secrets/{secret_name}',
+                    {
+                        owner: owner,
+                        repo: repo,
+                        environment_name: environmentName,
+                        secret_name: name,
+                        headers: {
+                            'X-GitHub-Api-Version': '2022-11-28'
+                        }
+                    }
+                )
+                const status = response.status
+
+                if (status === 200) {
+                    // Update
+                    await octokit.request(
+                        'PATCH /repos/{owner}/{repo}/environments/{environment_name}/secrets/{secret_name}',
+                        {
+                            owner: owner,
+                            repo: repo,
+                            environment_name: environmentName,
+                            secret_name: name,
+                            value: value,
+                            headers: {
+                                'X-GitHub-Api-Version': '2022-11-28'
+                            }
+                        }
+                    )
+                } else {
+                    // Create
+                    await octokit.request(
+                        'POST /repos/{owner}/{repo}/environments/{environment_name}/secrets',
+                        {
+                            owner: owner,
+                            repo: repo,
+                            environment_name: environmentName,
+                            secret_name: name,
+                            value: value,
+                            headers: {
+                                'X-GitHub-Api-Version': '2022-11-28'
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 module.exports = {
-  run
+    run
 }
---
+++ src/util.js
@@ -0,0 +1,22 @@
+const sodium = require('libsodium-wrappers')
+
+// Encrypt the value using libsodium
+export async function encrypt(key, value) {
+    //Check if libsodium is ready and then proceed.
+    sodium.ready.then(() => {
+        // Convert the secret and key to a Uint8Array.
+        const binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL)
+        const binsec = sodium.from_string(value)
+
+        // Encrypt the secret using libsodium
+        const encBytes = sodium.crypto_box_seal(binsec, binkey)
+
+        // Convert the encrypted Uint8Array to Base64
+        const encrypted_value = sodium.to_base64(
+            encBytes,
+            sodium.base64_variants.ORIGINAL
+        )
+
+        return encrypted_value
+    })
+}
