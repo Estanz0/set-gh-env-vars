c7ae628b8679b7f341a2222e3b109adc67069fc8
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2024-03-24 20:12:49 +1300

    build

--- bundle_and_tag.sh
+++ bundle_and_tag.sh
@@ -1,7 +1,15 @@
+
+
+git pull
+git tag -d v1.0
+git push --follow-tags
+
+git pull
+
+rm -rf dist
 npm run bundle
 
 git add .
 git commit -m "build"
-git tag -d v1.0
 git tag -a -m "First attempt" v1.0
 git push --follow-tags
